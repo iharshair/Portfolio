f85b459ff9928d152df07b555e02aa9e3461a50f
iharshair <iharshair@gmail.com>
2025-09-29 12:23:40 +0530

    Add files via upload

---
+++ portfolio/code1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class AscendingArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Take input for array size
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        // Take input for array elements
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Sort array using Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // Print sorted array
+        System.out.println("Array in Ascending Order:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
